b2b54b9cbfd57135bb3bb046ccc6829e664caf25
Irina <irina.lysenko21@gmail.com>
2020-06-18 01:37:27 +0300

    Updates

---
+++ precache-manifest.9e6b244370a27727e0a52433a4bc08c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065bea251aa7721cb19ea9101a48e7b6",
+    "url": "/cache-app/index.html"
+  },
+  {
+    "revision": "91e8329c13d495265d1b",
+    "url": "/cache-app/static/css/main.18264713.chunk.css"
+  },
+  {
+    "revision": "ac164bfe64dc13beace1",
+    "url": "/cache-app/static/js/2.b0e721c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cache-app/static/js/2.b0e721c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e8329c13d495265d1b",
+    "url": "/cache-app/static/js/main.918f86cf.chunk.js"
+  },
+  {
+    "revision": "80363f7bc3002a1be4af",
+    "url": "/cache-app/static/js/runtime-main.f2a868de.js"
+  }
+]);
